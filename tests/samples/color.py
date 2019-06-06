cd4f0eab0b12c851b1a383a797764e75b32f9451
Alex Hall <alex.mojaki@gmail.com>
2019-06-06 21:59:23 +0200

    Test indentation with color using plain_prefix

--- tests/samples/color.py
+++ tests/samples/color.py
@@ -15,7 +15,8 @@ def foo():
 @snoop(depth=2)
 def main():
     try:
-        foo()
+        (None
+         or foo)()
     except:
         pass
 
@@ -32,16 +33,18 @@ expected_output = """
 [90m12:34:56.78 [0m[36m[1m>>> Call to [0mmain[36m[1m in [0mFile "/path/to_file.py", line 16
 [90m12:34:56.78 [0m[90m  16[0m | def main():
 [90m12:34:56.78 [0m[90m  17[0m |     try:
-[90m12:34:56.78 [0m[90m  18[0m |         foo()
+[90m12:34:56.78 [0m[90m  18[0m |         (None
+[90m12:34:56.78 [0m[90m  19[0m |          or foo)()
 [90m    12:34:56.78 [0m[36m[1m>>> Call to [0mfoo[36m[1m in [0mFile "/path/to_file.py", line 11
 [90m    12:34:56.78 [0m[90m  11[0m | def foo():
 [90m    12:34:56.78 [0m[90m  12[0m |     raise TypeError
 [90m    12:34:56.78 [0m[31m[1m!!! TypeError[0m
 [90m    12:34:56.78 [0m[31m[1m!!! Call ended by exception[0m
-[90m12:34:56.78 [0m[90m  18[0m |         foo()
+[90m12:34:56.78 [0m[90m  19[0m |          or foo)()
 [90m12:34:56.78 [0m[31m[1m!!! TypeError[0m
-[90m12:34:56.78 [0m[31m[1m!!! When calling: [0mfoo()
-[90m12:34:56.78 [0m[90m  19[0m |     except:
-[90m12:34:56.78 [0m[90m  20[0m |         pass
+[90m12:34:56.78 [0m[31m[1m!!! When calling: [0m(None
+[90m12:34:56.78 [0m                  or foo)()
+[90m12:34:56.78 [0m[90m  20[0m |     except:
+[90m12:34:56.78 [0m[90m  21[0m |         pass
 [90m12:34:56.78 [0m[32m[1m<<< Return value from main: [0mNone
 """
