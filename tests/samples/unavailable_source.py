ccc2c4ee061509a5eea016f5eded923f9d09f48f
Alex Hall <alex.mojaki@gmail.com>
2019-09-03 20:28:30 +0200

    Use comment color from pygments style as grey for columns

--- tests/samples/unavailable_source.py
+++ tests/samples/unavailable_source.py
@@ -18,8 +18,8 @@ expected_output = """
 12:34:56.78    4 | SOURCE IS UNAVAILABLE
 12:34:56.78    5 | SOURCE IS UNAVAILABLE
 12:34:56.78 <<< Return value from foo: 3
-[90m12:34:56.78 [0m[36m[1m>>> Call to foo in File "/path/to_file.py", line 4[0m
-[90m12:34:56.78 [0m[90m   4[0m | SOURCE IS UNAVAILABLE
-[90m12:34:56.78 [0m[90m   5[0m | SOURCE IS UNAVAILABLE
-[90m12:34:56.78 [0m[32m[1m<<< Return value from foo: [0m[38;5;141m3[39m
+[38;5;242m12:34:56.78 [0m[36m[1m>>> Call to foo in File "/path/to_file.py", line 4[0m
+[38;5;242m12:34:56.78 [0m[38;5;242m   4[0m | SOURCE IS UNAVAILABLE
+[38;5;242m12:34:56.78 [0m[38;5;242m   5[0m | SOURCE IS UNAVAILABLE
+[38;5;242m12:34:56.78 [0m[32m[1m<<< Return value from foo: [0m[38;5;141m3[39m
 """
