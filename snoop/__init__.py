405ae459fcf6a2d4d13a78df165fdfa12f77d1a1
Alex Hall <alex.mojaki@gmail.com>
2020-08-25 23:31:11 +0200

    Bump to 0.2.5

--- snoop/__init__.py
+++ snoop/__init__.py
@@ -21,7 +21,7 @@ import sys
 __VersionInfo = collections.namedtuple('VersionInfo',
                                        ('major', 'minor', 'micro'))
 
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 __version_info__ = __VersionInfo(*(map(int, __version__.split('.'))))
 
 config = Config()
