98102bde87d092640828590927ef144d069dc56f
Alex Hall <alex.mojaki@gmail.com>
2022-12-22 15:48:56 +0200

    update make_release.sh

--- make_release.sh
+++ make_release.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
-set -eux
 
-# Ensure that there are no uncommitted changes
-# which would mess up using the git tag as a version
-[ -z "$(git status --porcelain)" ]
+set -eux -o pipefail
 
-if [ -z "${1+x}" ]
-then
+if [ ! -z "$(git status --porcelain)" ]; then
+    set +x
+    echo You have uncommitted changes which would mess up the git tag
+    exit 1
+fi
+
+if [ -z "${1+x}" ]; then
     set +x
     echo Provide a version argument
     echo "${0} <major>.<minor>.<patch>"
     exit 1
-else
-    if [[ ${1} =~ ^([0-9]+)(\.[0-9]+)?(\.[0-9]+)?$ ]]; then
-        :
-    else
-        echo "Not a valid release tag."
-        exit 1
-    fi
 fi
 
-tox -p auto
+if [[ ! ${1} =~ ^([0-9]+)(\.[0-9]+)?(\.[0-9]+)?$ ]]; then
+    echo "Not a valid release tag."
+    exit 1
+fi
+
+tox -p 3
 
 export TAG="v${1}"
 git tag "${TAG}"
-git push origin master "${TAG}"
+git push origin HEAD "${TAG}"
 rm -rf ./build ./dist
 python -m build --sdist --wheel .
 twine upload ./dist/*.whl dist/*.tar.gz
