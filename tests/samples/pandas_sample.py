40e0f4e4bbea4642a835507d9fca01468430dfc7
Alex Hall <alex.mojaki@gmail.com>
2026-01-11 14:51:05 +0200

    fix pandas sample

--- tests/samples/pandas_sample.py
+++ tests/samples/pandas_sample.py
@@ -5,8 +5,12 @@ import pandas as pd
 
 @snoop.snoop()
 def main():
-    arr = np.arange(10000)
+    arr = np.arange(10000).astype(str)
     arr = arr.reshape((100, 100))
     df = pd.DataFrame(arr)
     series = df[0]
     return series
+
+
+if __name__ == '__main__':
+    main()
